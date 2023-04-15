55ce0f13a0a0efc9d70f78e58fa1215ec1f256ee
dchang74 <NELSONC924@GMAIL.COM>
2023-04-15 14:09:12 -0400

    refactoring

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -11,8 +11,6 @@ import { DocumentAnalysisClient } from "@azure/ai-form-recognizer";
 const key = import.meta.env.VITE_AZURE_API_KEY;
 const endpoint = import.meta.env.VITE_AZURE_END_POINT;
 
-console.log(key, endpoint)
-
 async function main(url) {
   // sample document
   const formUrl = url;
@@ -43,10 +41,7 @@ function App() {
       document.getElementById('error-msg').innerHTML = "Please enter URL"
       return;
     }
-    // let file = document.querySelector('#test-file')
-    // let url = URL.createObjectURL(file.files[0]).split(':')[1]
-    // setLoading(!loading)
-    // console.log(loading)
+
     let loader = document.querySelector('.loader-background')
     loader.classList.remove('off')
     // console.log(file.files)
@@ -54,23 +49,7 @@ function App() {
 
     results.actions.setSyllabusResults(keyValuePairs)
     loader.classList.add('off')
-    // if (!keyValuePairs || keyValuePairs.length <= 0) {
-    //   console.log("No key-value pairs were extracted from the document.");
-    // } else {
-    //   console.log("Key-Value Pairs:", keyValuePairs);
-    //   for (const { key, value, confidence } of keyValuePairs) {
-    //     console.log("- Key  :", `"${key.content}"`);
-    //     console.log(
-    //       "  Value:",
-    //       `"${(value && value.content) || "<undefined>"}" (${confidence})`
-    //     );
-    //   }
-    // }
 
-
-    //testGPT()
-    // let file = document.querySelector('#upload-input')
-    // console.log(file.files[0].name)
     navigate('/results')
   };
   return (
@@ -83,9 +62,7 @@ function App() {
         <p>Enter the url to your syllabus below</p>
         <input type="text" id="test-url" placeholder="URL"/>
         <p id="error-msg"></p>
-        {/* <input type="file" id="test-file" placeholder="Upload a file" /> */}
-        {/* <DragDrop /> */}
-        {/* <button type='submit' onClick={(e)=> handleClick(e)}>submit</button> */}
+
         <button
           onClick={(e) => handleClick(e)}
           className="btn"
